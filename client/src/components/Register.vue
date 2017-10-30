011e20fe3e3e4d495a7efb7147a0ab3b68f5088b
taoxm310 <taoxiumin2016@gmail.com>
2017-10-30 11:35:09 +0800

    register with vuetify

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,12 +1,35 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input type="email" name="email" placeholder="email"
-    v-model="email"><br>
-    <input type="password" name="password" placeholder="password"
-    v-model="password"><br>
-    <button
-    @click="register">Register</button>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="teal lighten-3" dark>
+          <v-toolbar-title >Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input 
+            type="email" 
+            name="email" 
+            placeholder="email"
+            v-model="email"
+          ><br>
+          <input 
+            type="password" 
+            name="password" 
+            placeholder="password"
+            v-model="password"
+          ><br>
+          <div class="error" v-html="error">
+          </div>
+          <v-btn
+            @click="register"
+            class="teal lighten-3">
+            Register
+          </v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
+  
   </div>
 </template>
 
@@ -18,15 +41,20 @@ export default {
   data () {
     return {
       email: 'aaa',
-      password: '123'
+      password: '123',
+      error: null
     }
   },
   methods: {
     async register () {
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -34,5 +62,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.error {
+  color:red;
+}
 </style>
--- server/src/policies/AuthenticationControllerPolicy.js
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -8,6 +8,27 @@ module.exports = {
         new RegExp('^[A-Za-z0-9]{8,32}$')
       )
     }
+
+    const { error } = Joi.validate(req.body, schema)
+    if (error) {
+      switch (error.details[0].context.key) {
+        case 'email':
+          res.status(400).send({
+            error: 'a valid email address is a must'
+          })
+          break
+        case 'password':
+          res.status(400).send({
+            error: `password failed, at least 8 charactor with lower case, upper case, number`
+          })
+          break
+        default:
+          res.status(400).send({
+            error: 'Invalid information'
+          }
+          )
+      }
+    }
     next()
   }
 }
